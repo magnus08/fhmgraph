3457c5d8d7a22a4561ba255029c14932affa77e0
Magnus Andersson <magnus08@gmail.com>
2020-05-17 15:26:16 +0200

    Experimenting with excel reading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import readXlsxFile from "read-excel-file";
 
+const read = (file) => {
+  readXlsxFile(file, {sheet: 1}).then((rows) => {
+    const titles = rows[0].slice(1);
+    const index = titles.findIndex(t => t === "Stockholm") + 1;
+    rows.map(r => {
+      console.log(`Date: ${r[0]}, Num: ${r[index]}`);
+    });
+    console.log("Index: ", index);
+    // `rows` is an array of rows
+    // each row being an array of cells.
+  })
+};
+
+
+// input.addEventListener('change', () => {
+//   readXlsxFile(input.files[0]).then((rows) => {
+//     // `rows` is an array of rows
+//     // each row being an array of cells.
+//   })
+// })
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input type="file" onChange={(e) => read(e.target.files[0])}/>
         </p>
         <a
           className="App-link"
