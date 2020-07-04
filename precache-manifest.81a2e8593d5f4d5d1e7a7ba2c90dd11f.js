ed4eb329373b5552a2c2cbc1cb2a0ff1b357b843
Magnus Andersson <magnus.andersson@learnifier.com>
2020-07-04 09:54:12 +0200

    Updates

---
+++ precache-manifest.81a2e8593d5f4d5d1e7a7ba2c90dd11f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eb0ad25a85289ce640e12dde3ecd33b",
+    "url": "/fhmgraph/index.html"
+  },
+  {
+    "revision": "37cf30bda3eee9b6e52f",
+    "url": "/fhmgraph/static/css/main.f2ea30be.chunk.css"
+  },
+  {
+    "revision": "a7997f3d43fce17ab194",
+    "url": "/fhmgraph/static/js/2.c0d130b3.chunk.js"
+  },
+  {
+    "revision": "1938aa8702435f138be9250d407c3628",
+    "url": "/fhmgraph/static/js/2.c0d130b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37cf30bda3eee9b6e52f",
+    "url": "/fhmgraph/static/js/main.8f717b15.chunk.js"
+  },
+  {
+    "revision": "7837b173e60dd74b8200",
+    "url": "/fhmgraph/static/js/runtime-main.1728ec83.js"
+  }
+]);
